707d9e28919a248c9a9601e627efcad5b86ee0c5
kaioruan <kaio.ruan@souunit.com.br>
2022-09-14 15:44:04 -0300

    Feat: create middleware post product validation

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,27 @@
 import express from 'express';
-// import { getAllProducts } from './controllers/productsController';
 import ProductsController from './controllers/productsController';
 import UserController from './controllers/userController';
 import OrderController from './controllers/orderController';
 import LoginValidation from './middlewares/loginValidation';
+import ProductValidation from './middlewares/productValidation';
 
 const productController = new ProductsController();
 const userController = new UserController();
 const orderController = new OrderController();
 const loginValidation = new LoginValidation();
+const productValidation = new ProductValidation();
 const app = express();
 
 app.use(express.json());
 app.get('/products', productController.getAllProduct);
-app.post('/products', productController.create);
+app.post(
+  '/products',
+  productValidation.length,
+
+  productValidation.product,
+
+  productController.create,
+);
 app.get('/orders', orderController.getAllOrder);
 app.post('/users', userController.create);
 app.post('/login', loginValidation.loginV, userController.login);
